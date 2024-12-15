c8d0abe2e9707167edf247c5094d0dc2d9a607b7
Uriel Aguirre <uriel.flores1410@gmail.com>
2024-12-15 00:23:28 -0600

    create remote repo

---
+++ src/main/java/com/aluracursos/Litaralura/LitaraluraApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Litaralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitaraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitaraluraApplication.class, args);
+	}
+
+}
